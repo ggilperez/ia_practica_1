97e2076b6d573c4f18a8e9d783ff60d0ef18d4f1
Guillermo Gilperez <ggilperezalcazar@gmail.com>
2024-11-06 10:00:06 +0100

    heuristica arreglada

--- ia_practica_1/utils/chart_creator.py
+++ ia_practica_1/utils/chart_creator.py
@@ -29,7 +29,7 @@ def create_byb_chart():
     # ax2.tick_params(axis='y', labelcolor='red')
 
     # Títulos y leyenda
-    plt.title("Tiempo de ejecución por tamaño de tablero")
+    plt.title("Tiempo de ejecución por tamaño de tablero B&B")
     fig.tight_layout()
 
     # Crear el directorio si no existe
@@ -40,5 +40,41 @@ def create_byb_chart():
     fig.savefig(img_path)
 
     plt.show()
+    
+def create_astar_chart():
+    # Datos de la tabla
+    tableros = ["2x2", "3x3", "3x5", "5x5", "8x8"]
+    tiempos = [0.001, 0.003, 0.034, 5*60, 5*60]
+    # caballos = [4, 4, 5, 10, 24]
+
+    # Crear la figura y los ejes
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+
+    # Gráfico de barras para el tiempo
+    ax1.bar(tableros, tiempos, color='blue', alpha=0.7, label="Tiempo (s)")
+    # ax1.plot(tableros, tiempos, color='red', marker='o', label="Tiempo (s)")
+    ax1.set_xlabel("Tablero")
+    ax1.set_ylabel("Tiempo (s)", color='blue')
+    ax1.tick_params(axis='y', labelcolor='blue')
+
+    # Crear un segundo eje para la cantidad de caballos
+    # ax2 = ax1.twinx()
+    # ax2.plot(tableros, caballos, color='red', marker='o', label="Caballos")
+    # ax2.set_ylabel("Caballos", color='red')
+    # ax2.tick_params(axis='y', labelcolor='red')
+
+    # Títulos y leyenda
+    plt.title("Tiempo de ejecución por tamaño de tablero A*")
+    fig.tight_layout()
+
+    # Crear el directorio si no existe
+    img_path = imgs_path / "grafico_tiempo_caballos_astar.png"
+    os.makedirs(os.path.dirname(img_path), exist_ok=True)
+
+    # Guardar el gráfico como archivo PNG en el directorio actual
+    fig.savefig(img_path)
+
+    plt.show()
 
 create_byb_chart()
+create_astar_chart()
